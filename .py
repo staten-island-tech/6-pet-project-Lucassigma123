84b709e8c087dcfbe67337af8a68956e5c8313ae
Lucassigma123 <lucass126@nycstudents.net>
2025-11-03 10:17:09 -0500

    Update .py

---
+++ .py
@@ -0,0 +1,8 @@
+class pet:
+    def __init__(self, name, money, happiness, hunger):
+        self.name=name
+        self.money= int(money)
+        self.happiness=happiness
+        self.hunger= int(hunger)
+
+  
