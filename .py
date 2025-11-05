60346cc5e66a57571f2f9a59f9dd954ae5774f3e
Lucassigma123 <lucass126@nycstudents.net>
2025-11-05 10:15:29 -0500

    Update .py

--- .py
+++ .py
@@ -1,11 +1,13 @@
-class driddy:
+class pet:
     def __init__(self, name, money, happiness, hunger):
         self.name=name
         self.money= int(money)
         self.happiness=happiness
-        self.hunger= int(hunger)
+        self.hunger=(hunger) 
         
 def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
-     
+Driddy= pet("Driddy",175,"Sword")
+Driddy.buy({"title": "Pizza","health":2})     
+print (Driddy.__dict__)
