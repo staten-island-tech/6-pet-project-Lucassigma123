2a37459aaf7e8434956740493fab4b0d339dc6f6
Lucassigma123 <lucass126@nycstudents.net>
2025-11-14 10:15:07 -0500

    Update .py

--- .py
+++ .py
@@ -10,18 +10,23 @@ class pet:
     def buy(self, item):
           self.inventory.append(item)
           print(self.inventory)
-Driddy= pet("Driddy",175,50,50)
+Driddy= pet("Driddy",175,50,50,50)
 Driddy.buy({"title": "Pizza","hunger":10})    
 print (Driddy.__dict__)
 while True:
         x= input("play with driddy yes to play no to no play")
-        if x.lower()== "yes":
+        if x.lower()== "yes": 
           Driddy.happiness+=10
+          Driddy.energy-=10
         if Driddy.happiness>=100:
           Driddy.happiness=100
         print(f"driddy happy level", {Driddy.happiness})
+        print(f"Energy", {Driddy.energy})
         if x.lower()== "no":
           break
+        if Driddy.energy<=0:
+         print("game over Driddy died of exhaustion")
+         quit()
 
 while True:
     y= input("feed driddy yes to feed no to starve type continue to continue")
@@ -41,6 +46,14 @@ while True:
     
     
     if y.lower()=="continue":
+        break
+
+while True:
+    sleep=input("type sleep to let driddy rest for 1 hour")
+    if sleep.lower()==("sleep"):
+        Driddy.energy+=10
+    if Driddy.energy>=100:
+        Driddy.energy=100
         break
     
 
