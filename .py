d67e60a36e595a1832802fbd0c4b831cb44bf4b3
Lucassigma123 <lucass126@nycstudents.net>
2025-11-20 10:17:07 -0500

    Update .py

--- .py
+++ .py
@@ -1,3 +1,4 @@
+
 class pet:
     def __init__(self, name, money, happiness, hunger, energy):
         self.name=name
@@ -11,6 +12,7 @@ class pet:
           self.inventory.append(item)
           print(self.inventory)
 Driddy= pet("Driddy",175,50,50,50)
+
 Driddy.buy({"title": "Pizza","hunger":10})    
 print (Driddy.__dict__)
 def dead():
@@ -22,61 +24,66 @@ def dead():
 def play():
     while True:
         dead()
-        x= input("play with driddy yes to play no to no play")
-        if x.lower()== "yes": 
+        x= input("1: play 2: no play")
+        if x.lower()== "1": 
           Driddy.happiness+=10
           Driddy.energy-=10
         if Driddy.happiness>=100:
           Driddy.happiness=100
         print(f"driddy happy level", {Driddy.happiness})
         print(f"Energy", {Driddy.energy})
-        if x.lower()== "no":
+        if x.lower()== "2":
           break
+       
         
 def run():
     while True:
       dead()
-      y= input("feed driddy yes to feed no to starve type continue to continue")
-      if y.lower()=="yes":
+      y= input("1: feed 2: starve 3: continue")
+      if y=="1":
         Driddy.hunger+=10
-      if y.lower()=="no":
+      if y=="2":
         print("The end Driddy ate you")
         quit()
-      
-   
       if Driddy.hunger>=100:
         Driddy.hunger=100
         print("Driddy is full")
+        break
       print("hunger", Driddy.hunger)
-    
-    
-      if y.lower()=="continue":
-         break
+      if y=="3":
+          break
+
+
 def aaa():
     while True:
      dead()
-     sleep=input("type sleep to let driddy rest for 1 hour no to no rest")
-     if sleep.lower()==("sleep"):
+     sleep=input("1: sleep 2: leave")
+     if sleep==("1"):
         Driddy.energy+=10
+        Driddy.money+=10
      print ("Energy",Driddy.energy)
+     print("money",Driddy.money)
      if Driddy.energy>=100:
-        Driddy.energy=100
-        print("driddy is energized")
-        break
-     if sleep.lower()=="no":
+       Driddy.energy=100
+       print("driddy is energized")
+       break
+     if sleep=="2":
         break
 
 
 def sigma1():    
  while True:
   dead()
-  workout=input("type workout to workout no to no workout")
-  if workout.lower()=="workout":
+  workout=input("1: workout 2:leave")
+  if workout=="1":
      Driddy.energy-=25
-  print("energy",Driddy.energy)
-  if workout.lower()=="no":
+  elif workout=="2":
      break
 
+  print("energy",Driddy.energy)
+  
+def shopping():
+   l=input(1:skittles )
 while True:
   main={
          1:"play",
@@ -93,6 +100,8 @@ while True:
       aaa()
   elif sigma=="4":
       sigma1()
+  else:
+     print("INVALID OPTION READ")
 
 
     
